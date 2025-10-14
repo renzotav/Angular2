d40707ea7723d689d6a4f183b7eb49f533b31005
Renzo Tavares <renzotavaresmkn@gmail.com>
2025-10-14 10:32:14 -0300

    Adcionando lógica ao formulário

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button'
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-cadastro',
@@ -23,5 +24,11 @@ import { MatButtonModule } from '@angular/material/button'
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+
+cliente: Cliente = Cliente.newCliente();
+
+salvar(){
+  console.log("Dados do cliente", this.cliente);
+}
 
 }
