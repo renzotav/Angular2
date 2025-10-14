3ae41e30fcd5a7a572a4ff858b389c081187352a
Renzo Tavares <renzotavaresmkn@gmail.com>
2025-10-14 10:00:58 -0300

    Criando formulario

---
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button'
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+     ],
+    
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent {
+
+}
